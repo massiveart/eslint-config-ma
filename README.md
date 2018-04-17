aee6e8045db78155b64851da9aeb3a8745b9b444
Alexander Schranz <alexander.schranz@sulu.io>
2018-04-17 17:55:41 +0200

    add initialize eslint configuration (#1)

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # eslint-config-ma
+
+![CircleCi](https://circleci.com/gh/massiveart/eslint-config-ma/tree/master.png)
+
+Stylelint shareable config used by MASSIVE ART.
+
+## Installation
+
+To make use of this config, install this package as development dependency of your project:
+
+```bash
+npm install eslint-config-ma --save-dev
+```
+
+## Usage
+
+Create a [`.eslintrc`](https://eslint.org/docs/user-guide/configuring) config file:
+
+### .eslintrc
+
+```js
+{
+    "extends": "eslint-config-ma"
+}
+```
+
+## Version Update & Publish to NPM
+
+### 1. Create release on github
+
+Update package.json version on master branch:
+
+```bash
+git checkout master
+git pull origin master
+npm version [ major | minor | patch ] --no-git-tag-version
+git add .
+git commit -m "Release <version>"
+git push origin master
+```
+
+Generate changelog:
+
+```bash
+github_changelog_generator --future-release <version>
+```
+
+Copy the text of the last release into and get new release.
+
+### 2. Publish release
+
+```
+git fetch --tags
+git checkout <version>
+npm publish
+```
+
---
+++ index.js
@@ -0,0 +1,130 @@
+"use strict"
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": false,
+        "es6": true
+    },
+    "globals": {
+        "require": true,
+        "module": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4,
+            {
+                "SwitchCase": 1
+            }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "one-var": [
+            "error",
+            "never"
+        ],
+        "operator-linebreak": [
+            "error",
+            "before"
+        ],
+        "padded-blocks": [
+            "error",
+            "never"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "max-len": [
+            "error",
+            120
+        ],
+        "max-statements-per-line": [
+            "error",
+            {
+                "max": 1
+            }
+        ],
+        "no-multiple-empty-lines": [
+            "error",
+            {
+                "max": 1
+            }
+        ],
+        "object-curly-spacing": [
+            "error",
+            "always"
+        ],
+        "object-curly-newline": [
+            "error",
+            {
+                "minProperties": 1
+            }
+        ],
+        "space-before-function-paren": [
+            "error",
+            "never"
+        ],
+        "curly": "error",
+        "eqeqeq": "error",
+        "capitalized-comments": "error",
+        "no-unused-expressions": "error",
+        "no-useless-call": "error",
+        "no-useless-concat": "error",
+        "no-useless-escape": "error",
+        "no-useless-return": "error",
+        "vars-on-top": "error",
+        "no-undef-init": "error",
+        "no-undefined": "error",
+        "no-use-before-define": "error",
+        "callback-return": "error",
+        "no-mixed-requires": "error",
+        "array-bracket-spacing": "error",
+        "block-spacing": "error",
+        "brace-style": "error",
+        "camelcase": "error",
+        "comma-spacing": "error",
+        "comma-style": "error",
+        "comma-dangle": "error",
+        "computed-property-spacing": "error",
+        "eol-last": "error",
+        "func-call-spacing": "error",
+        "func-name-matching": "error",
+        "key-spacing": "error",
+        "keyword-spacing": "error",
+        "lines-around-directive": "error",
+        "new-cap": "error",
+        "new-parens": "error",
+        "newline-before-return": "error",
+        "no-lonely-if": "error",
+        "no-nested-ternary": "error",
+        "no-trailing-spaces": "error",
+        "no-underscore-dangle": "error",
+        "no-unneeded-ternary": "error",
+        "no-whitespace-before-property": "error",
+        "object-property-newline": "error",
+        "space-before-blocks": "error",
+        "space-in-parens": "error",
+        "space-infix-ops": "error",
+        "space-unary-ops": "error",
+        "spaced-comment": "error",
+        "no-compare-neg-zero": "error",
+        "no-negated-condition": "warn",
+        "no-warning-comments": "warn",
+        "global-require": "warn",
+        "require-jsdoc": "warn"
+    }
+}
+
