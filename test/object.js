31c0dccce736e05f270ae8b3274829ba8174ae15
Alexander Schranz <alexander@sulu.io>
2019-06-13 21:07:18 +0200

    Add new object style for import and exports

---
+++ test/object.js
@@ -0,0 +1,15 @@
+import { import1, import2 } from 'test';
+import import3 from 'test-import/with-very-long-name/with-very-long-name/with-very-long-name/with-very-long-name/with-very-long-name';
+
+var test1 = {};
+
+var test2 = {
+    import1,
+    import2,
+    import3
+};
+
+export {
+    test1 as Test1,
+    test2 as Test2
+};
